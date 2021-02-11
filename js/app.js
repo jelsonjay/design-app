1ed77bf63d3d6b3d31ad40ecc41d1a7d1b1e7919
Jelson J <jelson20@outlook.com>
2021-02-11 12:55:50 +0000

    update css

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 "use strict"
 
+window.addEventListener('scroll', function() {
+  const nav = document.querySelector('nav')
+  nav.classList.toggle('sticky', window.scrollY > 0)
+  })
+  
+  function toggleMenu(){
+  const menuToggle = document.querySelector('.menuToggle')
+  const menu = document.querySelector('.menu')
+  menuToggle.classList.toggle('active')
+  menu.classList.toggle('active')
+  }
